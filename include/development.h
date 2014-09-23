7258e74e65d295803f76677c98cdaa7e19652435
myun2 <myun2@nwhite.info>
2014-09-24 01:22:10 +0900

    development.h

---
+++ include/development.h
@@ -0,0 +1,19 @@
+#ifndef __github_com_myun2__city_simulator__development__H__
+#define __github_com_myun2__city_simulator__development__H__
+
+inline unsigned int road()
+{
+	return 20;
+}
+
+inline unsigned int rail()
+{
+	return 40;
+}
+
+inline unsigned int station()
+{
+	return 200;
+}
+
+#endif//__github_com_myun2__city_simulator__development__H__
