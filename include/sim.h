886247b076d33cee69d87e3cc4ace7fdf239baec
myun2 <myun2@nwhite.info>
2014-09-23 23:29:44 +0900

    include/sim.h Add.

---
+++ include/sim.h
@@ -0,0 +1,8 @@
+#ifndef __github_com_myun2__city_simulator__sim__H__
+#define __github_com_myun2__city_simulator__sim__H__
+
+struct sim_region
+{
+};
+
+#endif//__github_com_myun2__city_simulator__sim__H__
