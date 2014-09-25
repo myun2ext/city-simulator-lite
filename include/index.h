f08d7ff1c9be2ade35fdb8f00e400243ccdfe308
myun2 <myun2@nwhite.info>
2014-09-26 01:33:44 +0900

    index_item_t

--- include/index.h
+++ include/index.h
@@ -6,22 +6,28 @@
 
 typedef struct
 {
-	block* residencials;
-	block* commercials;
-	block* industrials;
-	block* roads;
-	block* rails;
-	block* administratives;
+	unsigned int x;
+	unsigned int y;
+} index_item_t;
+
+typedef struct
+{
+	index_item_t* residencials;
+	index_item_t* commercials;
+	index_item_t* industrials;
+	index_item_t* roads;
+	index_item_t* rails;
+	index_item_t* administratives;
 } index_t, index;
 
 inline void init_index(index *i, unsigned int initial_size)
 {
-	i->residencials = calloc(initial_size, sizeof(block_type));
-	i->commercials = calloc(initial_size, sizeof(block_type));
-	i->industrials = calloc(initial_size, sizeof(block_type));
-	i->roads = calloc(initial_size, sizeof(block_type));
-	i->rails = calloc(initial_size, sizeof(block_type));
-	i->administratives = calloc(initial_size, sizeof(block_type));
+	i->residencials = calloc(initial_size, sizeof(index_item_t));
+	i->commercials = calloc(initial_size, sizeof(index_item_t));
+	i->industrials = calloc(initial_size, sizeof(index_item_t));
+	i->roads = calloc(initial_size, sizeof(index_item_t));
+	i->rails = calloc(initial_size, sizeof(index_item_t));
+	i->administratives = calloc(initial_size, sizeof(index_item_t));
 }
 
 #endif//__github_com_myun2__city_simulator__index__H__
